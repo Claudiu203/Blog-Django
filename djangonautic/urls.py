5e9cf0ec3da481607e4e76f3e40683520cc3c8c6
Claudiu203 <mirel.claudiu2013@yahoo.ro>
2021-09-25 15:05:10 +0300

    deployment

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -4,7 +4,9 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-from articles import views as article_views 
+from articles import views as article_views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +14,8 @@ urlpatterns = [
     path('about/', views.about),
     path('', article_views.article_list, name= 'home'),
     path('accounts/', include('accounts.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
